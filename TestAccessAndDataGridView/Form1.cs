b9e5e838cd27531d626f0f90281de4aa647246c5
AUGUST <1094221167@qq.com>
2020-08-05 11:13:30 +0800

    v001

---
+++ TestAccessAndDataGridView/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//注释：     先ctrl+k，然后ctrl+c
+//取消注释： 先ctrl+k，然后ctrl+u
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\02Development\winform\WindowsFormsApp1\shop.accdb"); //Jet OLEDB:Database Password=
+            OleDbCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "select * from shop,student";
+            conn.Open();
+            OleDbDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            if (dr.HasRows)
+            {
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    dt.Columns.Add(dr.GetName(i));
+                }
+                dt.Rows.Clear();
+            }
+            while (dr.Read())
+            {
+                DataRow row = dt.NewRow();
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    row[i] = dr[i];
+                }
+                dt.Rows.Add(row);
+            }
+            cmd.Dispose();
+            conn.Close();
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
